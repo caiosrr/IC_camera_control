091951671cbfb14234e52fc1a0ec904464502c1f
Caio <caiodesr@usp.br>
2024-06-14 18:12:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import cv2
+import serial as serial
+
 
 webcam = cv2.VideoCapture(1) #int: 0 - webcam pc; 1 - webcam adicional.
 
 if webcam.isOpened():
-    validacao, frame = webcam.read()
-    # webcam.read() devolve a informação em booleano se a câmera esta conectada, armazenando em validacao, e um array de frames (print) em lista RGB, armazenado em frame
+    validacao, frame = webcam.read() #devolve em booleano se a câmera está conectada
+    #webcam.read() devolve a informação em booleano se a informação está sendo lida, armazenando em validacao, e um array de frames (print) em lista RGB, armazenado em frame
+    img_counter = 0  # Inicialize o contador
     while validacao:
         validacao, frame = webcam.read()
-        cv2.imshow("Vide da Webcam", frame)
-        # as imagens estão sendo processadas rapidamente, o que impede a visualização do vídeo. Vamos adicionar uma variável "key", que faz a imagem aguardar (milissegundos) e ainda armazena as teclas pressionadas durante o vídeo.
-        key = cv2.waitKey(5)
-        if key == 27: #ESC
+        cv2.imshow("Video da Webcam", frame)
+        key = cv2.waitKey(5)  # as imagens estão sendo processadas rapidamente, o que impede a visualização do vídeo. Vamos adicionar uma variável "key", que faz a imagem aguardar (milissegundos) e ainda armazena as teclas pressionadas durante o vídeo.
+        if key == 112:  # p minúsculo
+            img_name = "Frame_{}.png".format(img_counter)  # Crie um nome de arquivo único
+            cv2.imwrite(img_name, frame)  # Salve o frame com o novo nome de arquivo
+            img_counter += 1  # Incremente o contador
+        if key == 27:  # ESC
             break
-    cv2.imwrite("Frame.png", frame) #comando iwrite salva um frame, no local que esta sendo usado criará um arquivo png com o último frame registrado no vídeo.                   
 
 webcam.release()
-cv2.destroyAllWindows() #comandos para fechar a execução da webcam
+cv2.destroyAllWindows()
